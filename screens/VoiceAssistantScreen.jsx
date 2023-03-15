87c1c27d816b78583c55711d8f9c03aa042289a7
Muhammad Usama <liontiger49@yahoo.com>
2023-03-15 22:49:44 +0500

    Update VoiceAssistantScreen.jsx
    moved open ai API inside the function

--- screens/VoiceAssistantScreen.jsx
+++ screens/VoiceAssistantScreen.jsx
@@ -12,7 +12,7 @@ import Voice from '@react-native-voice/voice';
 import Logo from '../assets/logo.png'
 import ChatMessage from '../components/ChatMessage';
 
-const OPENAI_API = "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";
+
 
 const VoiceAssistantScreen = () => {
   const [chatList,setChatList] = useState([])
@@ -177,26 +177,27 @@ const VoiceAssistantScreen = () => {
   }
 
   const getResponseFromOpenAI = async (text) => {
-      const headers = {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${OPENAI_API}`
-      }
-      const data = {
-        prompt:text,
-        model:'text-davinci-003',
-        temperature: 0.7,
-        max_tokens: 100,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-        stop:['.']
-      }
-      try{
-        const openai = await axios.post('https://api.openai.com/v1/completions',data,{headers:headers});
-        return openai.data.choices[0].text.trim()
-      }catch (err) {
-        console.log(err);
-      }
+    const OPENAI_API = "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";
+    const headers = {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${OPENAI_API}`
+    }
+    const data = {
+      prompt:text,
+      model:'text-davinci-003',
+      temperature: 0.7,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      stop:['.']
+    }
+    try{
+      const openai = await axios.post('https://api.openai.com/v1/completions',data,{headers:headers});
+      return openai.data.choices[0].text.trim()
+    }catch (err) {
+      console.log(err);
+    }
   }
 
   const sendCommand = async (messageFromArgs) => {
